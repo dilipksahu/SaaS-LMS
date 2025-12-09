143ab9b7879ac32507c13dfe7e59dcbcafbb3027
dilip.sahu <anil.jain.con@hubblehox.com>
2025-12-09 19:13:50 +0530

    prisma setup,connection and migrate created

--- lms-backend/src/app.module.ts
+++ lms-backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { EnrollmentsModule } from './enrollments/enrollments.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CoursesModule, EnrollmentsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
